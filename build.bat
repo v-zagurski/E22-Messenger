3539a7b8984ae6ae1ed634d1c02766e292708533
V. Zagurski <arbeitersolidaritat@gmail.com>
2026-05-29 11:08:58 +0300

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,14 @@
+nuitka ^
+--remove-output ^
+--onefile ^
+--onefile-cache-mode=cached ^
+--onefile-tempdir-spec="{TEMP}/e22cache" ^
+--windows-console-mode=disable ^
+--windows-icon-from-ico=init.ico ^
+--output-filename="E22-Messenger" ^
+--product-name="E22-Messenger" ^
+--product-version=1.0 ^
+--enable-plugin=pyside6 ^
+--mingw64 ^
+--lto=yes ^
+"E22messenger.py"
